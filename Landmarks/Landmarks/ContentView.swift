72f5a580ef28b9d971ec760d6875571222c3be63
Nick Payne <ndpayne@gmail.com>
2022-02-10 18:32:14 +0000

    pull it all together

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -10,24 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
-                Text("Peak District")
-                    .font(.title)
-                HStack {
-                    Text("Stanage Edge")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("Derbyshire")
-                        .font(.subheadline)
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading) {
+                VStack(alignment: .leading) {
+                    Text("Peak District")
+                        .font(.title)
+                    HStack {
+                        Text("Stanage Edge")
+                            .font(.subheadline)
+                        Spacer()
+                        Text("Derbyshire")
+                            .font(.subheadline)
+                    }
+                    
+                    Divider()
+                    
+                    Text("About Stanage Edge")
+                        .font(.title2)
+                    Text("What an amazing place to climb.")
+                    
                 }
+                .padding()
             }
-            .padding()
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
