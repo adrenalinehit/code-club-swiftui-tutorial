f36dcf7ff4369a89a4643b5413f2c699646d7518
Nick Payne <ndpayne@gmail.com>
2022-02-10 18:28:58 +0000

    maps

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Nick Payne on 10/02/2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 53.349_545, longitude: -1.630_183),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
