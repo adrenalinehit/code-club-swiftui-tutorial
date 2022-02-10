ef4311edfca5618e7f5a8ffd920b31106ea5eee5
Nick Payne <nick.payne2@skybettingandgaming.com>
2022-02-10 17:37:59 +0000

    setup

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Nick Payne on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
