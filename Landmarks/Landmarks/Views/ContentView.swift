70374b9f8f4d9cd4db4ea45a66626af43ed10e35
Nick Payne <ndpayne@gmail.com>
2022-02-17 17:35:36 +0000

    list and navigation created

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -9,38 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                VStack(alignment: .leading) {
-                    Text("Peak District")
-                        .font(.title)
-                        
-                    HStack() {
-                        Text("Stanage Edge")
-                            .font(.subheadline)
-                        Spacer()
-                        Text("Derbyshire")
-                            .font(.subheadline)
-                    }
-                    
-                    Divider()
-                    
-                    Text("About Stanage Edge")
-                        .font(.title2)
-                    Text("What an amazing place to climb.")
-                    
-                }
-                .padding()
-            }
-        }
+        LandmarkList()
     }
     
     struct ContentView_Previews: PreviewProvider {
